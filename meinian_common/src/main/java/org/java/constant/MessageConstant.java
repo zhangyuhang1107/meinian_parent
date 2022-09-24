638686df0bea4c976b9421040ea5658082a3b5cf
zhangyuhang <2788671582@qq.com>
2022-09-24 12:53:31 +0800

    新增修改套餐消息常量

--- meinian_common/src/main/java/org/java/constant/MessageConstant.java
+++ meinian_common/src/main/java/org/java/constant/MessageConstant.java
@@ -28,6 +28,8 @@ public class MessageConstant {
     public static final String ADD_SETMEAL_SUCCESS = "新增套餐成功";
     public static final String DELETE_SETMEAL_SUCCESS = "删除套餐成功";
     public static final String DELETE_SETMEAL_FAIL = "删除套餐失败";
+    public static final String UPDATE_SETMEAL_SUCCESS = "修改套餐成功";
+    public static final String UPDATE_SETMEAL_FAIL = "修改套餐失败";
     public static final String IMPORT_ORDERSETTING_FAIL = "批量导入预约设置数据失败";
     public static final String IMPORT_ORDERSETTING_SUCCESS = "批量导入预约设置数据成功";
     public static final String GET_ORDERSETTING_SUCCESS = "获取预约设置数据成功";
