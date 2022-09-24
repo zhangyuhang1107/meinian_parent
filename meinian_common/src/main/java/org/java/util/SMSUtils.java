0e4d00fad569218404f7a26b1cae1bb4dbba8598
zhangyuhang <2788671582@qq.com>
2022-09-24 08:56:54 +0800

    第三方短信发送工具类

--- meinian_common/src/main/java/org/java/util/SMSUtils.java
+++ meinian_common/src/main/java/org/java/util/SMSUtils.java
@@ -21,7 +21,6 @@ public class SMSUtils {
         String host = "http://dingxin.market.alicloudapi.com";
         String path = "/dx/sendSms";
         String method = "POST";
-//        String appcode = "f6c24716fcda48578948f1fdedb6b70a";
         String appcode = "";
         Map<String, String> headers = new HashMap<String, String>();
         //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
